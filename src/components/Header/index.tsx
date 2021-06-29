a0df9c0da58d93ea3e36c2f501340e85605885eb
gv4383 <gregory.vargas18@gmail.com>
2021-06-28 22:31:06 -0500

    Create basic header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Header = () => (
+  <header className="header-container">
+    <div className="header-sub-container">
+      <div className="header-logo">saatva</div>
+      <div>cart</div>
+    </div>
+  </header>
+);
+
+export default Header;
