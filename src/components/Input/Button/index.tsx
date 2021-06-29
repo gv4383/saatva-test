620251e69b5144a2a83a12b93b8c07a9b70189f7
gv4383 <gregory.vargas18@gmail.com>
2021-06-29 16:39:20 -0500

    Add styling for selected buttons

--- src/components/Input/Button/index.tsx
+++ src/components/Input/Button/index.tsx
@@ -4,17 +4,23 @@ import './styles.scss';
 
 interface Props {
   isSelectButton?: boolean;
+  isSelected?: boolean;
   onClick: () => void;
   text: string;
 }
 
 const Button = (props: Props) => {
-  const { isSelectButton, onClick, text } = props;
+  const { isSelectButton, isSelected, onClick, text } = props;
 
   const getClassNames = () => {
     const baseClassName = 'button-container';
     const normalClassName = 'normal-button';
     const selectClassName = 'select-button';
+    const selectedClassName = 'select-button-selected';
+
+    if (isSelectButton && isSelected) {
+      return `${baseClassName} ${selectedClassName}`;
+    }
 
     if (isSelectButton) {
       return `${baseClassName} ${selectClassName}`;
